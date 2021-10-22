277cb75e1580b1632d5aa47f5dac4a757ad4c199
hpollard003 <hpollard003@gmail.com>
2021-10-22 16:22:11 -0400

    User table and initial migrations

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+    def index
+        render json: User.all , status: :ok
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
